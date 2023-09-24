aacaa0dceb7c90730c3c7a07ac5c0f821c00c8e9
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2023-09-24 20:43:07 +1000

    Created module files for different tabs

---
+++ src/home.js
@@ -0,0 +1,55 @@
+export function loadHomeTab(main) {
+    let h1 = document.createElement('h1');
+    h1.innerText = 'Oporto';
+    main.appendChild(h1);
+
+    let div = document.createElement('div');
+    div.classList.add('intro');
+    let p = document.createElement('p');
+    p.innerText = "Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.";
+    div.appendChild(p);
+    let h2 = document.createElement('h2');
+    h2.innerText = 'Australian';
+    div.appendChild(h2);
+    main.appendChild(div);
+
+    div = document.createElement('div');
+    div.classList.add('hours');
+    h2 = document.createElement('h2');
+    h2.innerText = 'Hours';
+    div.appendChild(h2);
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
+    li.innerText = 'Sunday: 8am - 8pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Monday: 6am - 6pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Tuesday: 6am - 6pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Wednesday: 6am - 6pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Thursday: 6am - 10pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Friday: 6am - 10pm';
+    ul.appendChild(li);
+    li = document.createElement('li');
+    li.innerText = 'Saturday: 8am - 10pm';
+    ul.appendChild(li);
+    div.appendChild(ul);
+    main.appendChild(div);
+
+    div = document.createElement('div');
+    div.classList.add('location');
+    h2 = document.createElement('h2');
+    h2.innerText = 'Location'
+    div.appendChild(h2);
+    p = document.createElement('p');
+    p.innerText = '123 Forest Drive, Forestville, Maine';
+    div.appendChild(p);
+    main.appendChild(div);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Icon from './oporto.png';
+import { loadHomeTab } from './home';
 
 const content = document.querySelector('#content');
 
@@ -24,8 +25,8 @@ function loadPage() {
     content.appendChild(header)
 
     const main = document.createElement('main');
-    loadHomeTab(main);
     content.appendChild(main);
+    loadHomeTab(main);
 
     const footer = document.createElement('footer');
     ul = document.createElement('ul');
@@ -41,62 +42,6 @@ function loadPage() {
     nav.appendChild(ul);
     footer.appendChild(ul);
     content.appendChild(footer);
-}
-
-function loadHomeTab(main) {
-    let h1 = document.createElement('h1');
-    h1.innerText = 'Oporto';
-    main.appendChild(h1);
-
-    let div = document.createElement('div');
-    div.classList.add('intro');
-    let p = document.createElement('p');
-    p.innerText = "Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.";
-    div.appendChild(p);
-    let h2 = document.createElement('h2');
-    h2.innerText = 'Australian';
-    div.appendChild(h2);
-    main.appendChild(div);
-
-    div = document.createElement('div');
-    div.classList.add('hours');
-    h2 = document.createElement('h2');
-    h2.innerText = 'Hours';
-    div.appendChild(h2);
-    let ul = document.createElement('ul');
-    let li = document.createElement('li');
-    li.innerText = 'Sunday: 8am - 8pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Monday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Tuesday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Wednesday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Thursday: 6am - 10pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Friday: 6am - 10pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Saturday: 8am - 10pm';
-    ul.appendChild(li);
-    div.appendChild(ul);
-    main.appendChild(div);
-
-    div = document.createElement('div');
-    div.classList.add('location');
-    h2 = document.createElement('h2');
-    h2.innerText = 'Location'
-    div.appendChild(h2);
-    p = document.createElement('p');
-    p.innerText = '123 Forest Drive, Forestville, Maine';
-    div.appendChild(p);
-    main.appendChild(div);
 }
 
 loadPage();
