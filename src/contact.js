318efc5126eec1f4332f3cefae88f282b81f05d2
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2023-09-25 21:13:09 +1000

    Completed Contact page

--- src/contact.js
+++ src/contact.js
@@ -10,52 +10,22 @@ export function loadContactTab(main) {
     main.appendChild(h1);
 
     let div = document.createElement('div');
-    div.classList.add('intro');
-    let p = document.createElement('p');
-    p.innerText = "Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.";
-    div.appendChild(p);
+    div.classList.add('location');
     let h2 = document.createElement('h2');
-    h2.innerText = 'Australian';
+    h2.innerText = 'Find Locations here!'
     div.appendChild(h2);
-    main.appendChild(div);
-
-    div = document.createElement('div');
-    div.classList.add('hours');
-    h2 = document.createElement('h2');
-    h2.innerText = 'Hours';
-    div.appendChild(h2);
-    let ul = document.createElement('ul');
-    let li = document.createElement('li');
-    li.innerText = 'Sunday: 8am - 8pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Monday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Tuesday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Wednesday: 6am - 6pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Thursday: 6am - 10pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Friday: 6am - 10pm';
-    ul.appendChild(li);
-    li = document.createElement('li');
-    li.innerText = 'Saturday: 8am - 10pm';
-    ul.appendChild(li);
-    div.appendChild(ul);
+    let p = document.createElement('p');
+    p.innerText = '123 Forest Drive, Forestville, Maine';
+    div.appendChild(p);
     main.appendChild(div);
 
     div = document.createElement('div');
     div.classList.add('location');
     h2 = document.createElement('h2');
-    h2.innerText = 'Location'
+    h2.innerText = 'Call us on'
     div.appendChild(h2);
     p = document.createElement('p');
-    p.innerText = '123 Forest Drive, Forestville, Maine';
+    p.innerText = '1234567890';
     div.appendChild(p);
     main.appendChild(div);
 }
